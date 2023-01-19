f15e0570ea78621b6bff5ae561419593066d9dfb
sithond <sithond@DESKTOP-178HJ8G>
2023-01-19 22:34:43 +0300

    Добавьте файлы проекта.

---
+++ Currency exchanger/Mod/AppData.cs
@@ -0,0 +1,14 @@
+﻿using Currency_exchanger.Mod;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Currency_exchanger
+{
+    public static class AppData
+    {
+        public static testEntities2 db = new testEntities2();
+    }
+}
---
+++ Currency exchanger/Page/AccPage.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Currency_exchanger.Page
+{
+    /// <summary>
+    /// Логика взаимодействия для AccPage.xaml
+    /// </summary>
+    public partial class AccPage
+    {     
+        public AccPage()
+        {
+            InitializeComponent();          
+        }
+
+        private void accBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var CurrentUser = AppData.db.logins.FirstOrDefault(l => l.login1 == textBox_login.Text && l.pass == password.Text);
+            if (CurrentUser != null)
+            {
+                MessageBox.Show("Норм!");
+            }
+            else
+            {
+                MessageBox.Show("Плохо!");
+            }
+        }
+
+    }
+}
